5e291a49e2e39208ebad76c5d6233ce9eca7dcd4
James Morgan <MorganJay@users.noreply.github.com>
2021-02-13 07:49:56 +0100

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import './App.scss';
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 
+import './App.scss';
+
+const StationeriesPage = props => (
+  <div>
+    {console.log(props)}
+    <h1>Stationeries Page</h1>
+    {/* <Link to="/">Home</Link> */}
+    <button onClick={() => props.history.push('/')}>Topics</button>
+  </div>
+);
+
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/stationeries" component={StationeriesPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,28 +12,28 @@ class Directory extends Component {
           imageUrl:
             'https://images.unsplash.com/photo-1456735190827-d1262f71b8a3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1048&q=80://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'shop/stationeries',
+          linkUrl: 'stationeries',
         },
         {
           title: 'OFFICE EQUIPMENT',
           imageUrl:
             'https://images.unsplash.com/photo-1483058712412-4245e9b90334?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80.co/px2tCc3/jackets.png',
           id: 2,
-          linkUrl: 'shop/office',
+          linkUrl: 'office',
         },
         {
           title: 'COMPUTER ACCESSORIES',
           imageUrl:
             'https://images.unsplash.com/photo-1551739440-5dd934d3a94a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fGNvbXB1dGVyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
-          linkUrl: 'shop/computers',
+          linkUrl: 'computers',
         },
         {
           title: 'PRINTERS AND ACCESSORIES',
           imageUrl:
             'https://images.unsplash.com/photo-1612815154858-60aa4c59eaa6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
           id: 4,
-          linkUrl: 'shop/printers',
+          linkUrl: 'printers',
         },
         {
           title: 'NETWORKING',
@@ -41,7 +41,7 @@ class Directory extends Component {
             'https://images.unsplash.com/photo-1544197150-b99a580bb7a8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
-          linkUrl: 'shop/networking',
+          linkUrl: 'networking',
         },
         {
           title: 'POS/INVENTORY SOFTWARE',
@@ -49,7 +49,7 @@ class Directory extends Component {
             'https://images.unsplash.com/photo-1586864030223-a918b07d357d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1225&q=80://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 6,
-          linkUrl: 'shop/pos-inventory',
+          linkUrl: 'pos-inventory',
         },
       ],
     };
@@ -57,8 +57,8 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...sectionProps }) => (
+          <MenuItem key={id} {...sectionProps} />
         ))}
       </div>
     );
