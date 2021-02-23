332e021ae8c79286998fbfaa8dad4e785c0220fd
James Morgan <MorganJay@users.noreply.github.com>
2021-02-24 00:34:32 +0100

    Updating Stripe Integration and Favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import Logo from '../../assets/img/DIVINE-GLOBAL.png'
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51IO9dOBTCm5l4rq8dz1VyxxGqhIAnu3dLNIC6s7cpEedyrSlNb4cTDaw6CPusIp7GExtX47LhKOPDVwyExQ0Vhpd00YhKrDdi4';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful')
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Divine Global Store"
+      currency="NGN"
+      billingAddress
+      shippingAddress
+      image={Logo}
+      description={`Your total is NGN${price} `}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
 
 import {
   selectCartItems,
@@ -37,6 +39,12 @@ const CheckoutPage = ({ cartItems, total, clearAllItems }) => (
     <div className="total">
       <span>TOTAL: &#8358;{total}</span>
     </div>
+    <div className="text-warning">
+      *Please use the following test credit card for payment*
+      <br/>
+      4242 4242 4242 4242 - Exp: 05/21 -CVV 123
+    </div>
+    <StripeCheckoutButton price={total}/>
   </div>
 );
 
