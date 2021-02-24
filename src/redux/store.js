fc7a8dd92472f4421536f24be2e7c414d14b7a4c
James Morgan <MorganJay@users.noreply.github.com>
2021-02-24 17:55:23 +0100

    Still Managing Logger in Production App

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,8 +6,10 @@ import rootReducer from './root-reducer';
 
 const middleWares = [];
 
-if (process.env.NODE_ENV === 'development') middleWares.push(logger);
+if (process.env.NODE_ENV === 'development') {
+  middleWares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
-export const persistor = persistStore(store); 
+export const persistor = persistStore(store);
